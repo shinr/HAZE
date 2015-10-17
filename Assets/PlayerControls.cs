ad5fecb50af2212ededd80f9df43b36f0b78a459
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2015-10-17 17:44:58 +0300

    initial commit
    initial

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+    public float speed;
+    CharacterController controller;
+    
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float multiplier = 1.0f;
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            multiplier = 4.0f;
+        }
+        controller.SimpleMove(multiplier * speed * transform.forward * verticalAxis + transform.right * speed * horizontalAxis* multiplier);
+	}
+}
---
+++ Assets/Teleporter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject pair;
+    public bool receiveOnly;
+    bool receiving = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!receiving && !receiveOnly)
+        {
+            pair.GetComponent<Teleporter>().ReceiveTeleport(other, transform.position);
+        }
+        receiving = false;
+    }
+
+    public void ReceiveTeleport(Collider other, Vector3 origin)
+    {
+        receiving = true;
+        Vector3 newPosition = transform.position - (origin - other.gameObject.transform.position);
+        other.gameObject.transform.position = newPosition;
+        if (transform.rotation.eulerAngles != Vector3.zero)
+        {
+            other.gameObject.GetComponent<CameraControl>().RotateMouse(new Vector2(transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.x));
+            //            newPosition = Quaternion.AngleAxis(rotation.y, transform.up) * newPosition;
+            other.gameObject.transform.RotateAround(transform.position, transform.up, transform.rotation.eulerAngles.y);
+        }
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
